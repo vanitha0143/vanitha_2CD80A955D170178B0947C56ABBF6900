44578e686312470e43203796dc68d3e8f0729b30
vanitha0143 <144794651+vanitha0143@users.noreply.github.com>
2023-09-12 16:20:25 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,9 @@
+#Implement a recursive function to calculate the factorial of a given number
+def factorial(n):
+    return 1 if (n < 1) else n * factorial(n - 1)
+ 
+ 
+if __name__ == '__main__':
+ 
+    n = 6
+    print(f'The Factorial of {n} is', factorial(n))
